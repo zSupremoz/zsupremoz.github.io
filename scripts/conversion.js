60fde9bf4bf23d2ca5022b44337a10d5f554becd
No <erickilpatrick572@gmail.com>
2020-05-26 20:35:30 -0400

    Updated Prize Bulb conversions

--- scripts/conversion.js
+++ scripts/conversion.js
@@ -1,7 +1,7 @@
 /* Converts the specified XP amount to Prize Bulbs*/
 function convertToPrizeBulb () {
     var xpCount = document.getElementById("xp_amount").value;
-    var result = xpCount / 5000;
+    var result = xpCount / 4000;
     
     document.getElementById("prize_bulbs").value = result;
 }
@@ -9,7 +9,7 @@ function convertToPrizeBulb () {
 /* Converts the specified Prize Bulb amount to XP */
 function convertToXp () {
     var BulbCount = document.getElementById("prize_bulbs").value;
-    var result = BulbCount * 5000;
+    var result = BulbCount * 4000;
 
     document.getElementById("xp_amount").value = result;
 }
